78232470e5b6a1fd472a7e9872b5b5a2398f86e0
«Elena <e.sorokina761@gmail.com>
2023-06-28 11:57:34 +0400

    add Homework Lesson_6

---
+++ Homework_41/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 41
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+int [] CreateArray(int size)
+{
+    int [] myArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        myArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return myArray;
+}
+
+void FindQuantity (int [] existingArray)
+{
+    int count = 0;
+    for (int i = 0; i < existingArray.Length; i++)
+    {
+        
+        if (existingArray[i] > 0)
+        {
+            count = count +1;
+            
+        }
+        
+    }
+    Console.Write ($"Кол-во чисел больше нуля - {count}");
+}
+    
+    
+Console.Write ("Введите кол-во чисел ");
+int mySize = Convert.ToInt32(Console.ReadLine());
+int [] array = CreateArray(mySize);
+FindQuantity(array);
---
+++ Homework_43/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 43
+Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем
+*/
+
+void findDecision(double b1, double k1, double b2, double k2)
+{
+    double x;
+    double y;
+    if (k1 == k2 && b1 == b2)
+    {
+       Console.WriteLine("Прямые совпадают"); 
+    }
+    else
+        if (k1 == k2)
+        {
+            Console.WriteLine("Прямые параллельны");
+        }
+        else
+        {
+        x = (b2-b1)/(k1-k2);
+        y = k1*x + b1;
+        Console.WriteLine($"x is {x}, y is {y}");
+        }
+}
+
+Console.WriteLine ("Введите b1, k1, b2, k2");
+double myB1 = Convert.ToInt32(Console.ReadLine());
+double myK1 = Convert.ToInt32(Console.ReadLine());
+double myB2 = Convert.ToInt32(Console.ReadLine());
+double myK2 = Convert.ToInt32(Console.ReadLine());
+findDecision(myB1, myK1, myB2, myK2);
+
